a6ae6c8c3dc6fc73a21916633306ecd92374f5b9
Dapo Oloyede <oladapooloyede@gmail.com>
2022-10-26 14:24:30 -0600

    adding plugins for backstage catalog item discovery

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,12 +2,20 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addEntityProvider(
+     GithubEntityProvider.fromConfig(env.config, {
+       logger: env.logger,
+       scheduler: env.scheduler,
+     }),
+   );
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
